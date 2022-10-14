b831fa4dfdcab45986b5f813b144d20c0606f21a
adrian <adrian@INF4-PCProfe>
2022-10-14 20:43:30 +0200

    Aparecen

--- src/BucleDoWHile/Ejercicio09.java
+++ src/BucleDoWHile/Ejercicio09.java
@@ -4,7 +4,28 @@ public class Ejercicio09 {
 
 	public static void main(String[] args) {
 		// BUscar aparicion numero
-
+		int numero = 47523;
+		String cadenaSi = "";
+		String cadenaNo = "";
+		
+		for(int i=0;i<=9;i++) {
+			boolean aparece = false;
+			int aux = numero;
+			while(aux>0 && !aparece) {
+				int digito = aux% 10;
+				aux= aux / 10;
+				if(digito == i) {
+					aparece = true;
+				}
+			}
+			if(aparece) {
+				cadenaSi = cadenaSi + " " + i;
+			}else {
+				cadenaNo = cadenaNo + " " + i;
+			}
+		}
+		System.out.println("AParecen " + cadenaSi);
+		System.out.println("NO aparecen " + cadenaNo);
 	}
 
 }
