e8bf13486a8d6873996f253c3d3ce04d7ae64742
adrian <adrian@INF4-PCProfe>
2022-11-04 20:58:58 +0100

    estructura ejercicio camarero

--- src/metodos/Ejercicio04.java
+++ src/metodos/Ejercicio04.java
@@ -1,14 +1,49 @@
 package metodos;
 
+import java.util.Scanner;
+
 public class Ejercicio04 {
 
-	private int refrescos = 0;
-	private int bocatas = 0;
-	private int cervezas = 0;
+	private static int refrescos = 0;
+	private static int bocatas = 0;
+	private static int cervezas = 0;
+	
+	private static void total() {
+		
+	}
+	
+	private static void agregaUno(int prod) {
+		
+	}
+	
+	private static void agregaDos(int prod) {
+			
+		}
+	
+	private static void eliminaUno(int prod) {
+		
+	}
+	
+	private static void borrar(int prod) {
+		
+	}
+	
+	private static void acciones(int producto) {
+		//accion 1.añadir1 2. añadir2 3. quitar1 4. borrar
+		int opcion = 0;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int opcion = 0;
+		do {
+			Scanner teclado = new Scanner(System.in);
+			System.out.print("Dime el producto: ");
+			opcion = teclado.nextInt();
+			acciones(opcion);
+		}while(opcion != 4);
+		
+		total();
 	}
 
 }
